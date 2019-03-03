252b8ea01d1fa844837a8946e8aa4b420d8fbffa
Daen Rebel <daenrebel035@gmail.com>
2019-03-03 18:18:00 +0100

    It's still Daen ;)

--- src/App.vue
+++ src/App.vue
@@ -33,7 +33,7 @@ export default {
   data() {
     return {
       loaded: false,
-      user: "joost",
+      user: "daen",
       hostname: "portfolio",
       location: "~",
       inputData: "",
